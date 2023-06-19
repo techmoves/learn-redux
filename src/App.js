e4eb8a80ab93162ea6e74757070c41e82b20bee4
techmoves <muhndlovu28@gmail.com>
2023-06-20 00:50:15 +0200

    add cart itme

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./component/navbar";
+import CartContainer from "./feartures/cart/cartContainer";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
